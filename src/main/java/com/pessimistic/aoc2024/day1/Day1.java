66f4a0f6a853de88218f91a010a442dacd4960cc
Andreas Christianson <achristianson@pessimistic-it.com>
2024-12-01 23:09:48 -0600

    extract and simplify

--- src/main/java/com/pessimistic/aoc2024/day1/Day1.java
+++ src/main/java/com/pessimistic/aoc2024/day1/Day1.java
@@ -3,6 +3,7 @@ package com.pessimistic.aoc2024.day1;
 import com.pessimistic.aoc2024.FileUtils;
 import com.pessimistic.aoc2024.TextUtils;
 
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -10,15 +11,11 @@ public class Day1 {
     private Day1() {
     }
 
-    public static int similarityStar1(String fileName) {
-        var lines = FileUtils.readTestFile(fileName);
-        var combined = lines.stream()
-                .map(TextUtils::splitOnWhitespace)
-                .map(s -> s.map(Integer::parseInt).toList())
-                .toList();
+    public static Long similarityStar1(String fileName) {
+        var combined = getCombined(fileName);
         var left = combined.stream().map(l -> l.get(0)).sorted().toList();
         var right = combined.stream().map(l -> l.get(1)).sorted().toList();
-        var sum = 0;
+        var sum = 0L;
         for (var i = 0; i < combined.size(); i++) {
             sum += Math.abs(left.get(i) - right.get(i));
         }
@@ -26,18 +23,19 @@ public class Day1 {
     }
 
     public static long similarityStar2(String fileName) {
-        var lines = FileUtils.readTestFile(fileName);
-        var combined = lines.stream()
-                .map(TextUtils::splitOnWhitespace)
-                .map(s -> s.map(Integer::parseInt).toList())
-                .toList();
+        var combined = getCombined(fileName);
         var left = combined.stream().map(l -> l.get(0)).sorted().toList();
         var rightCounts = combined.stream()
                 .map(l -> l.get(1)).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        var sum = 0L;
-        for (var i = 0; i < combined.size(); i++) {
-            sum += left.get(i) * rightCounts.getOrDefault(left.get(i), 0L);
-        }
-        return sum;
+        return left.stream().mapToLong(i -> i * rightCounts.getOrDefault(i, 0L))
+                .sum();
+    }
+
+    private static List<List<Integer>> getCombined(String fileName) {
+        var lines = FileUtils.readTestFile(fileName);
+        return lines.stream()
+                .map(TextUtils::splitOnWhitespace)
+                .map(s -> s.map(Integer::parseInt).toList())
+                .toList();
     }
 }
