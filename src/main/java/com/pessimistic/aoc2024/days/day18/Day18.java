f988e4654b33c01923b6a208f84cd4e1a6cc9a67
Andreas Christianson <achristianson@pessimistic-it.com>
2024-12-18 01:07:50 -0600

    prep

---
+++ src/main/java/com/pessimistic/aoc2024/days/day18/Day18.java
@@ -0,0 +1,29 @@
+package com.pessimistic.aoc2024.days.day18;
+
+import com.pessimistic.aoc2024.days.day17.Device;
+import com.pessimistic.aoc2024.util.FileUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day18 {
+    private static final Logger logger = Logger.getLogger(Day18.class.getName());
+
+    private Day18() {
+    }
+
+    public static long star1(String fileName) {
+        var lines = FileUtils.readTestFile(fileName);
+        return -1;
+    }
+
+    public static long star2(String fileName) {
+        var lines = FileUtils.readTestFile(fileName);
+        return -1;
+    }
+}
---
+++ src/test/java/com/pessimistic/aoc2024/days/day18/Day18Test.java
@@ -0,0 +1,36 @@
+package com.pessimistic.aoc2024.days.day18;
+
+import com.pessimistic.aoc2024.days.day18.Day18;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day18Test {
+
+    @Test
+    void star1Example1() {
+        var result = Day18.star1("example/day-18.1.txt");
+        assertThat(result).isEqualTo(7036);
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
+    void star1() {
+        var result = Day18.star1("actual/day-18.txt");
+        assertThat(result).isEqualTo(82460);
+    }
+
+    @Test
+    void star2Example1() {
+        var result = Day18.star2("example/day-18.1.txt");
+        assertThat(result).isEqualTo(45);
+    }
+
+    @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
+    @Test
+    void star2() {
+        var result = Day18.star2("actual/day-18.txt");
+        assertThat(result).isEqualTo(590);
+    }
+}
